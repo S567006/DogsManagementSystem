9ecf25ddb67ce584d724234af6137e9bca710aa7
S567006 <143199322+S567006@users.noreply.github.com>
2024-04-17 17:39:43 -0500

    Add files via upload

---
+++ DogManagmentSystem/src/main/java/com/fellahClasses/DMS/DogManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fellahClasses.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogManagmentSystemApplication.class, args);
+	}
+
+}
